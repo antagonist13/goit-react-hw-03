739e96b678989c645465e0d56cb6066812c0f44a
antagonist13 <1703shakayaroslaw@gmail.com>
2024-05-30 20:54:53 +0300

    searcher finish

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,12 @@
-export default function SearchBox({value, onSearching}) {
-    return <div>
+import css from './SearchBox.module.css'
+export default function SearchBox({ value, onSearching }) {
+    return <div className={css.searchingBlock}>
       <p>Find contacts by name</p>
       <input
         type="text"
         value={value}
         onChange={(e) => onSearching(e.target.value)}
+        className={css.searchInput}
       />
     </div>
 }
