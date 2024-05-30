383a6ee92a29b0af0a18a9855f72b7c484da6af3
antagonist13 <1703shakayaroslaw@gmail.com>
2024-05-30 20:32:34 +0300

    contact list styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,16 @@
-export default function Contact({ contacts: {name, id, number}, deleteContact }) {
-    console.log(name);
+import css from './Contact.module.css'
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoPerson } from "react-icons/io5";
+export default function Contact({ contacts: { name, id, number }, deleteContact }) {
     return (
-    <div >
-        <p >{name}</p>
-        <p >{number}</p>
-      <button onClick={() => deleteContact(id)}>
+    <>
+        <div>
+            <p className={css.text}>{<IoPerson className={css.icon} />} {name}</p>
+            <p className={css.text}>{<FaPhoneAlt className={css.icon} />} {number}</p>
+        </div>
+      <button onClick={() => deleteContact(id)} className={css.removeButton}>
         Delete
       </button>
-    </div>
+    </>
   );
 }
